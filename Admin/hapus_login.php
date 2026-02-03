7b030debd89073861a97025b3e24a3822fc4a00e
TheBims7 <farizbimaprayoga@gmail.com>
2026-02-03 23:37:46 +0700

    Fix script alerts and standardize login.php case

--- Admin/hapus_login.php
+++ Admin/hapus_login.php
@@ -4,25 +4,23 @@ include("../database.php");
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Cek role terlebih dahulu
     $check = mysqli_query($conn, "SELECT * FROM login WHERE id = $id");
     $user = mysqli_fetch_assoc($check);
 
     if (!$user) {
-        echo "<script>alert('Aktifitas tidak ditemukan.'); window.location.href='Login.php';</script>";
+        echo "<script>alert('Aktivitas tidak ditemukan.'); window.location.href='login.php';</script>";
         exit();
     }
 
-    // Jika bukan admin, lanjut hapus
     $sql = "DELETE FROM login WHERE id = $id";
     $query = mysqli_query($conn, $sql);
 
     if ($query) {
-        echo "<script>alert('Aktifitas berhasil dihapus.'); window.location.href='Login.php';</script>";
+        echo "<script>alert('Aktivitas berhasil dihapus.'); window.location.href='login.php';</script>";
     } else {
-        echo "<script>alert('Gagal menghapus aktifitas.'); window.location.href='Login.php';</script>";
+        echo "<script>alert('Gagal menghapus aktifitas.'); window.location.href='login.php';</script>";
     }
 } else {
-    echo "<script>alert('Akses tidak sah.'); window.location.href='Login.php';</script>";
+    echo "<script>alert('Akses tidak sah.'); window.location.href='login.php';</script>";
 }
 ?>
